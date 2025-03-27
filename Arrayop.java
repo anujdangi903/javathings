0e9f9e770b7093509a2d2faa3b0b85b70b972e9e
Anuj Dangi <0808io231017.ies@ipsacademy.org>
2025-03-27 22:06:07 +0530

    Add files via upload

--- Arrayop.java
+++ Arrayop.java
@@ -1,28 +1,42 @@
 import java.util.Scanner;
 
-public class Arrayop{
-   static Scanner scn= new Scanner(System.in);
-   public static void main(String[] args){
-       int [] array= takeinput();
-       display(array);
+public class Arrayop {
+    static Scanner scn = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] array = takeinput();
+        display(array);
+        System.out.println(arrmax(array));
+
     }
-    public static int[] takeinput(){
-    System.out.println("size");
-    int n = scn.nextInt();
-    
-    int[] arr= new int[n];
-    for (int i =0;i<arr.length;i++){
-    arr[i]=scn.nextInt();
-}
-     return arr;
-    
+
+    public static int[] takeinput() {
+        System.out.println("size");
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        return arr;
+
     }
-    public static void display(int [] arr){
-        for (int i=0; i< arr.length; i++){
-        System.out.println(arr[i]);
+
+    public static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
+
+    public static int arrmax(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        // Corrected: Integer.MIN_VALUE instead of Interger.MIN_VALUE
+System.out.println("biggest");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) { // Corrected: compare arr[i] with max
+                max = arr[i];
+            }
+        }
+        return max;
     }
-    
-    
-    
 }
